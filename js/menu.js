0680abdd118ba341c3220e42aed74cc50692c567
fabian <zaratefabian@hotmail.com>
2022-01-11 19:58:57 -0300

    Funcionalidad Zoom a las imagenes

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,22 @@
 const menu = document.querySelector(".icon_menu");
+const iconClose = document.querySelector(".icon_close");
+
 const nav = document.querySelector(".navegacion");
+const img_meditaciones = document.querySelectorAll(".img_meditaciones");
+const contenedor_zoom = document.querySelector(".contenedor_zoom");
+const img_zoom =document.querySelector(".img_zoom")
 
+img_meditaciones.forEach( img =>{
+    img.addEventListener('click', function() {
+        const imgRuta= img.getAttribute("src");
+        img_zoom.setAttribute("src",imgRuta);
+        contenedor_zoom.classList.toggle("mostra_img_zoom")
+        })
+});
 
+iconClose.addEventListener('click', function() {
+    contenedor_zoom.classList.toggle("mostra_img_zoom")
+});
 
 menu.addEventListener('click', function() {
     nav.classList.toggle("mostrar_menu");
